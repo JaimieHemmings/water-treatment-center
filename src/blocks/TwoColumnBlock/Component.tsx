9d488cf3133a958352999dd74c297679f1cb1f24
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-01-27 22:33:17 +0000

    Update Component.tsx

--- src/blocks/TwoColumnBlock/Component.tsx
+++ src/blocks/TwoColumnBlock/Component.tsx
@@ -63,7 +63,7 @@ export const TwoColumnBlock: React.FC<TwoColumnBlockProps> = ({ contentleft, con
         <h2 className="block text-selectiveyellow font-semibold pb-5 animate-text-af29">
           {title}
         </h2>
-        {mainContent && <RichText data={mainContent} enableGutter={false} className="max-w-none prose md:prose-md mb-5 text-2xl md:text-5xl text-white mr-0 animate-text-af29" />}
+        {mainContent && <RichText data={mainContent} enableGutter={false} className="max-w-none prose md:prose-md text-2xl md:text-5xl text-white mr-0 animate-text-af29" />}
       </div>
       <div className="container pb-5 flex flex-col md:flex-row justify-between md:border-b-8 border-azul">
         <div className="md:basis-1/3 pt-5 md:p-5">
