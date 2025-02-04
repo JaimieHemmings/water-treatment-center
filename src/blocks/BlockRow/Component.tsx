186157b3fe6eb7d81bf1a25cbfe179a72ed84799
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-02-04 11:38:40 +0000

    Update Component.tsx

--- src/blocks/BlockRow/Component.tsx
+++ src/blocks/BlockRow/Component.tsx
@@ -1,32 +1,63 @@
-import Link from 'next/link';
 import React from 'react';
-import { FaHandHoldingWater } from "react-icons/fa";
+import { FaHandHoldingWater, FaWater } from "react-icons/fa";
+import { FaGlassWaterDroplet } from "react-icons/fa6";
+import { IoWaterSharp } from "react-icons/io5";
+import { LuGlassWater } from "react-icons/lu";
+import { GiWaterBottle, GiWaterGallon } from "react-icons/gi";
+import { CMSLink } from '@/components/Link';
 
-export const BlockRow: React.FC = () => {
+interface BlockRowProps {
+  items: Array<{
+    text: string;
+    link: string;
+    linkLabel: string;
+    icon: 'water' | 'filter' | 'test' | 'plant';
+  }>;
+}
+
+export const BlockRow: React.FC<BlockRowProps> = ({ items }) => {
  return (
   <section className="bg-darkblue py-[5rem] relative z-20">
     <div className="container flex flex-col md:flex-row justify-around gap-10 -mt-[10rem]">
-      <div className="bg-azul p-10 text-center flex flex-col gap-10 md:basis-1/3">
-        <FaHandHoldingWater className="mx-auto text-8xl" />
-        <h2 className="text-2xl md:text-4xl">This is a test</h2>
-        <Link href="/about">
-          Learn More
-        </Link>
-      </div>
-      <div className="bg-azul p-10 text-center flex flex-col gap-10 md:basis-1/3">
-        <FaHandHoldingWater className="mx-auto text-8xl" />
-        <h2 className="text-2xl md:text-4xl">This is a test</h2>
-        <Link href="/about">
-          Learn More
-        </Link>
-      </div>
-      <div className="bg-azul p-10 text-center flex flex-col gap-10 md:basis-1/3">
-        <FaHandHoldingWater className="mx-auto text-8xl" />
-        <h2 className="text-2xl md:text-4xl">This is a test</h2>
-        <Link href="/about">
-          Learn More
-        </Link>
-      </div>
+      {items.map((item, index) => {
+        const Icon = {
+          water: FaHandHoldingWater,
+          filter: FaWater,
+          test: FaGlassWaterDroplet,
+          plant: IoWaterSharp,
+        }[item.icon];
+
+        return (
+          <div key={index} className="bg-azul p-10 text-center flex flex-col md:basis-1/3 justify-between">
+            <Icon className="mx-auto text-8xl pb-5" />
+            <h2 className="text-2xl pb-5">{item.text}</h2>
+            <CMSLink url={item.link} appearance="link"
+              className="inline-flex
+                        items-center
+                        justify-center
+                        whitespace-nowrap
+                        font-medium
+                        transition-colors
+                        disabled:pointer-events-none
+                        disabled:opacity-50
+                        h-11 px-8 mt-3
+                        bg-white 
+                        text-azul
+                        border-2
+                        border-selectiveyellow
+                        hover:bg-darkblue
+                        hover:text-white
+                        text-lg
+                        relative
+                        z-30
+                        no-underline
+                        hover:no-underline"
+              >
+              {item.linkLabel}
+            </CMSLink>
+          </div>
+        );
+      })}
     </div>
   </section>
  )
