674661c40017727977ea7c3553fa9fdd19cc5f0d
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-02-15 11:56:13 +0000

    update: nav

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -1,146 +1,50 @@
-'use client'
 import Link from 'next/link'
-import React, { useState } from 'react'
-import Image from 'next/image'
+import React from 'react'
+import { MdPhone, MdEmail, MdLocationOn, MdWarehouse } from "react-icons/md";
+
 import type { Header } from '@/payload-types'
+
+import { Logo } from '@/components/Logo/Logo'
+import { HeaderNav } from './Nav'
 
 interface HeaderClientProps {
   data: Header
 }
 
-export const HeaderClient: React.FC<HeaderClientProps> = () => {
-  const [isNavVisible, setIsNavVisible] = useState(false)
-
-  const toggleNavVisibility = () => {
-    setIsNavVisible(!isNavVisible)
-  }
-
-  const navItems = [
-    { href: "/about", label: "About", subLabel: "Who We Are" },
-    { href: "/services", label: "Services", subLabel: "& Maintenance" },
-    { href: "/products", label: "Products", subLabel: "Water Purification" },
-    { href: "/news", label: "News", subLabel: "Latest Work" },
-    { href: "/contact", label: "Contact", subLabel: "Get In Touch", isSpecial: true }
-  ];
-
-  const handleNavClick = () => {
-    if (isNavVisible) {
-      toggleNavVisibility();
-    }
-  };
+export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
 
   return (
-    <header id="navbar" className="w-full z-50 sticky top-0 left-0 bg-darkblue border-azul border-b-2 text-white">
-      <nav className="relative flex flex-row justify-between container">
-        <Link
-        onClick={handleNavClick}
-        href="/" className="z-50 text-xs font-semibold px-3 py-2">
-          <Image 
-            src="/logo.webp" 
-            alt="The Water Treatment Centre" 
-            width={212} 
-            height={55} 
-            priority 
-            className="max-h-[50px] w-auto" 
-          />
+    <>
+    <div className="w-full bg-white hidden-sm md:block">
+      <div className="container py-2 flex justify-end items-center gap-4">
+        <div>
+          <p className="text-sm text-jet">
+            <MdPhone className="inline-block" />
+            {' '}
+            <a href="tel:0861715686" className="text-jet">
+              086 1715686
+            </a>
+          </p>
+        </div>
+        <div>
+          <p className="text-sm text-jet"><MdEmail className="inline-block" />{' '}
+            <a
+            href="mailto:info@thewatertreatmentcentre.ie"
+            className="text-jet">
+              info@thewatertreatmentcentre.ie
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <header className="bg-darkblue text-white top-0 left-0 w-full z-[9999] border-b-2 border-azul sticky">
+      <div className="flex justify-between container">
+        <Link href="/">
+          <Logo loading="eager" priority="high" className="invert dark:invert-0 py-3" />
         </Link>
-        
-        {/* Desktop Navigation */}
-        <div className="hidden md:flex flex-row">
-          {navItems.map(({ href, label, subLabel, isSpecial }) => (
-            <Link
-              key={href}
-              href={href} 
-              className={`
-                px-5 py-3 
-                uppercase 
-                flex flex-col 
-                justify-center 
-                border-t-4 
-                border-t-darkblue
-                hover:border-t-azul 
-                transition-all
-                text-sm 
-                md:flex-1
-                ${isSpecial ? 'bg-azul border-t-argentinian text-white hover:text-white' : ''}
-              `}
-            >
-              {label}
-              <span className="block uppercase font-semibold whitespace-nowrap">
-                {subLabel}
-              </span>
-            </Link>
-          ))}
-        </div>
-        
-        {/* Mobile Navigation */}
-        <div 
-          className={`
-            fixed 
-            z-40 
-            top-0 
-            left-0 
-            w-full 
-            h-screen 
-            bg-darkblue 
-            flex 
-            flex-col 
-            justify-center 
-            items-center 
-            md:hidden
-            transition-opacity 
-            duration-300 
-            ${isNavVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'}
-          `}
-        >
-          {navItems.map(({ href, label, subLabel, isSpecial }) => (
-            <Link 
-              onClick={handleNavClick}
-              key={href}
-              href={href} 
-              className={`
-                px-5 py-3
-                w-full 
-                uppercase 
-                flex flex-col 
-                justify-center 
-                border-t-4 
-                border-t-selectiveyellow 
-                hover:border-t-selectiveyellow 
-                transition-all
-                text-sm 
-                ${isSpecial ? 'bg-azul text-white hover:text-white' : ''}
-              `}
-            >
-              {label}
-              <span className="block uppercase font-semibold whitespace-nowrap">
-                {subLabel}
-              </span>
-            </Link>
-          ))}
-        </div>
-        
-        <button 
-          className="z-50 menu-burger flex flex-col justify-center items-center md:hidden" 
-          onClick={toggleNavVisibility}
-          aria-label="Toggle Navigation Menu"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 10 10"
-            stroke="#eee"
-            strokeWidth={0.6}
-            fill="rgba(0,0,0,0)"
-            strokeLinecap="round"
-            className="cursor-pointer w-[50px] h-[50px]"
-          >
-            <path d={isNavVisible 
-              ? "M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7" 
-              : "M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7"} 
-            />
-          </svg>
-        </button>
-      </nav>
+        <HeaderNav data={data} />
+      </div>
     </header>
+    </>
   )
 }
--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -1,43 +1,25 @@
-'use client'
-
 import React from 'react'
+
 import type { Header as HeaderType } from '@/payload-types'
+
+import { CMSLink } from '@/components/Link'
 import Link from 'next/link'
 
-export const HeaderNav: React.FC<{ data: HeaderType }> = () => {
+export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
+  const navItems = data?.navItems || []
+
+  const linkClasses = "font-semibold no-underline hover:no-underline hover:bg-azul px-3 h-full text-white text-sm flex items-center"
 
   return (
-    <nav className="flex flex-row gap-3 items-center">
-      <div>
-        <Link href="/">
-          Home
-        </Link>
-      </div>
-      <div>
-        <Link href="/">
-          Home
-        </Link>
-      </div>
-      <div>
-        <Link href="/">
-          Home
-        </Link>
-      </div>
-      <div>
-        <Link href="/">
-          Home
-        </Link>
-      </div>
-      <div>
-        <Link href="/">
-          Home
-        </Link>
-      </div>
-      <div>
-        <Link href="/">
-          Home
-        </Link>
-      </div>
+    <nav className="flex items-center">
+      {navItems.map(({ link }, i) => {
+        return (
+          <CMSLink key={i} {...link} appearance="link" className={linkClasses} />
+        )
+      })}
+      <Link href="/contact" className="font-semibold no-underline hover:no-underline px-3 h-full text-white text-sm flex items-center bg-azul hover:bg-teal">
+        Contact Us
+      </Link>
     </nav>
   )
 }
