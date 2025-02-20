eb5296d1925c6c84dbf46c2c7075413cd7b3b348
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-02-20 19:23:35 +0000

    update: review

--- src/blocks/ReviewBlock/Component.tsx
+++ src/blocks/ReviewBlock/Component.tsx
@@ -2,6 +2,7 @@ import SectionTitle from '@/components/SectionTitle'
 import React from 'react'
 import ReviewCarousel from './ReviewCarousel'
 import CustomLink from '@/components/CustomLink'
+import Image from 'next/image'
 
 interface ReviewBlockProps {
   title: string
@@ -18,7 +19,14 @@ const ReviewBlock: React.FC<ReviewBlockProps> = ({title, subtitle, reviews, yout
     <div className="py-[5rem]">
       <SectionTitle title={title} subtitle={subtitle} />
       <div className="container flex flex-col md:flex-row-reverse gap-10 py-5 pt-[3rem]">
-        <div className="md:basis-1/2">
+        <div className="md:basis-1/2 relative">
+          <Image 
+            src="/logo-image.png" 
+            width={150} 
+            height={150} 
+            alt="Logo" 
+            className="opacity-10 w-[50%] h-auto -z-1 right-0 bottom-0 absolute" 
+          />
           <ReviewCarousel reviews={reviews} />
           <CustomLink theme="white" label="Read More Reviews" link="https://www.google.com/search?client=firefox-b-d&q=thewatertreatment+centre+ireland#lrd=0x485daf9cb276c2eb:0x6d324757580b9ad2,1,,,," />
         </div>
--- src/blocks/ReviewBlock/ReviewCarousel.tsx
+++ src/blocks/ReviewBlock/ReviewCarousel.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import React, { useEffect } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
+import './embla.css'
 
 interface Review {
   review: string
@@ -15,38 +16,33 @@ interface ReviewCarouselProps {
 
 const ReviewCarousel: React.FC<ReviewCarouselProps> = ({ reviews }) => {
 
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 5000 })]);
-  
-  useEffect(() => {
-      if (emblaApi) {
-        // Embla API is ready
-      }
-    }, [emblaApi]);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ 
+    loop: true,
+    align: 'center',
+    skipSnaps: false,
+  }, [Autoplay({ delay: 5000 })]);
 
   return (
-    <div className="embla overflow-hidden" ref={emblaRef}>
-      <div className="embla__container flex">
-        {reviews?.map((review, index) => (
-          <div 
-            key={index} 
-            className="bg-antiflashwhite p-5 rounded-xl mb-5 relative prose w-full overflow-hidden embla__slide border-[5px] border-darkblue"
-          >
-            <div className='relative z-10 h-full'>
-              <p className="text-gray-700 prose md:prose-xl max-w-none">
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {reviews?.map((review, index) => (
+            <div 
+              key={index} 
+              className="prose embla__slide"
+            >
+              <p className="text-antiflashwhite prose md:prose-xl max-w-none">
                 {review.review}
               </p>
               <p className="text-2xl mt-5 text-teal font-semibold">
                 {review.reviewer}
               </p>
-              <div className="absolute w-full h-full bottom-[15px] right-[15px]">
-                <Image src="/logo-image.png" width={150} height={150} alt="Logo" className="opacity-10 w-full h-auto -z-1" />
-              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default ReviewCarousel
