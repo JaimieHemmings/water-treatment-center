96806e1dfa8065065e7008dad40b98d2138c1fff
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-02-22 11:21:39 +0000

    Update page.tsx

--- src/app/(frontend)/products/[slug]/[product]/page.tsx
+++ src/app/(frontend)/products/[slug]/[product]/page.tsx
@@ -7,8 +7,13 @@ import RichText from '@/components/RichText'
 import { generateMeta } from '@/utilities/generateMeta'
 import AnimateIn from '@/components/Animations/AnimateIn'
 import Link from 'next/link'
-import ProductHero from './ProductHero'
 import ProductCTA from './ProductCTA'
+import { IoIosCheckmark } from "react-icons/io";
+import { FaPeopleRoof } from "react-icons/fa6";
+import { FaEuroSign } from "react-icons/fa";
+import CustomLink from '@/components/CustomLink'
+import { GiAutoRepair } from "react-icons/gi";
+import { GrMoney } from "react-icons/gr";
 
 type Props = {
   params: Promise<{
@@ -23,22 +28,106 @@ export default async function Product({ params }: Props) {
   return (
     <>
     <article className="bg-darkblue relative z-0">
-      <ProductHero productData={productData} />
       <PageClient />
       <div className="flex flex-col items-center gap-4 py-[5rem]">
-        <div className="container">
-          <div className="px-[2rem]">
+        <div className="container flex flex-col md:flex-row gap-4 py-5">
+          <div className="md:basis-2/3">
+            {productData.productImage && (
+              <AnimateIn
+                animation={{
+                  y: 50,
+                  opacity: 0,
+                  rotate: -5,
+                  duration: 0.8,
+                }}>
+                    <Image
+                      src={productData.productImage.url}
+                      alt={productData.productImage.alt || 'No alt text available'}
+                      className="inset-0 w-full object-cover"
+                      width={productData.productImage.width}
+                      height={productData.productImage.height}
+                      priority
+                    />
+              </AnimateIn>
+            )}
+          </div>
+          <div className="md:basis-1/3">
             <AnimateIn animation={{
-              y: 50,
-              opacity: 0,
-              duration: 0.8,
-            }}>
-              <h2 className="text-2xl md:text-4xl font-semibold text-white pb-5 border-b-2 border-selectiveyellow mb-5">
-                {productData.title}
-              </h2>
-            </AnimateIn>
-          </div> 
+                y: 50,
+                opacity: 0,
+                duration: 0.8,
+              }}>
+                <h2 className="text-2xl md:text-4xl font-semibold text-white pb-5 border-b-2 border-azul mb-5 inline-block">
+                  {productData.title}
+                </h2>
+              </AnimateIn>
+              <p className="prose md:prose-md text-white">
+              High performance countertop dispenser for demanding, hygiene-focused environments.
+              </p>
+              <ul className='list-none list-inside text-white mt-4 flex flex-col gap-4 px-0 pb-5'>
+                <li className="text-white relative pl-[20px]">
+                  <IoIosCheckmark className="absolute top-[-5px] left-[-15px] p-0 m-0 text-4xl text-azul"/>
+                  Outstanding Firewall® UVC purification system
+                </li>
+                <li className="text-white relative pl-[20px]">
+                  <IoIosCheckmark className="absolute top-[-5px] left-[-15px] p-0 m-0 text-4xl text-azul"/>
+                  Compact and durable stainless steel build
+                </li>
+                <li className="text-white relative pl-[20px]">
+                  <IoIosCheckmark className="absolute top-[-5px] left-[-15px] p-0 m-0 text-4xl text-azul"/>
+                  Touch-free dispenser offering five water options
+                </li>
+              </ul>
+              <ul className="py-5 flex flex-col gap-4">
+                <li className="text-white relative pl-[20px]">
+                  <FaPeopleRoof className="absolute top-[-2px] left-[-12px] p-0 m-0 text-2xl text-azul"/>
+                  Suitable for households up to <strong>10 people</strong>
+                </li>
+                <li className="text-white relative pl-[20px]">
+                  <FaEuroSign className="absolute top-[-2px] left-[-12px] p-0 m-0 text-2xl text-azul"/>
+                  Get a FREE no obligation quote
+                </li>
+                <li className="text-white relative pl-[20px]">
+                  <GrMoney className="absolute top-[-2px] left-[-12px] p-0 m-0 text-2xl text-azul"/>
+                  Payment Plans Available
+                </li>
+                <li className="text-white relative pl-[20px]">
+                  <GiAutoRepair className="absolute top-[-2px] left-[-12px] p-0 m-0 text-2xl text-azul"/>
+                  Installation &amp; maintenance services available
+                </li>
+              </ul>
+          </div>
         </div>
+        <div className="w-full bg-antiflashwhite p-3 text-jet shadow-[0px_4px_6px_0px_rgba(74,_144,_226,_0.4)]">
+          <div className="container flex flex-row gap-4 justify-between align-middle items-center">
+            <ul className="flex flex-row gap-4 text-azul">
+              <li>
+                <Link className="font-semibold text-lg hover:border-b-2 hover:border-azul" href="#features">
+                  Features
+                </Link>
+              </li>
+              <li>
+                <Link className="font-semibold text-lg hover:border-b-2 hover:border-azul"  href="#features">
+                  Details
+                </Link>
+              </li>
+              <li>
+                <Link className="font-semibold text-lg hover:border-b-2 hover:border-azul"  href="#features">
+                  Services
+                </Link>
+              </li>
+            </ul>
+            <CustomLink label="Contact Us For A Quote" link="/products" theme="light" className='m-0' />
+          </div>
+        </div>
+        
+        
+
+
+
+
+
+
         <div className="container">
           <div className="flex flex-col md:flex-row gap-4">
             <div className="md:basis-1/3">
