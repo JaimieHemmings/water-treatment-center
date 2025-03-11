7cad2d7de5e4156e14d783c4834b951d5b326189
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-03-11 14:34:06 +0000

    Update Component.tsx

--- src/blocks/SplitTextBlock/Component.tsx
+++ src/blocks/SplitTextBlock/Component.tsx
@@ -3,7 +3,7 @@ import SplitText from '@/components/SplitText'
 
 const SplitTextBlock = ({ items }) => {
   return (
-    <section className="py-[2rem] bg-darkblue text-white">
+    <section className="py-[2rem] pb-[5rem] bg-darkblue text-white">
       <div className="container">
         {items.map((item, index) => (
           <SplitText key={index} item={item} index={index} />
