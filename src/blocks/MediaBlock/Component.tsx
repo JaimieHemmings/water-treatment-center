ccc3dc80b47ab453afd0e09ea5e8b90b1b64cc0b
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-03-12 16:16:32 +0000

    Update Component.tsx

--- src/blocks/MediaBlock/Component.tsx
+++ src/blocks/MediaBlock/Component.tsx
@@ -2,7 +2,6 @@ import type { StaticImageData } from 'next/image'
 
 import { cn } from 'src/utilities/cn'
 import React from 'react'
-import RichText from '@/components/RichText'
 
 import type { MediaBlock as MediaBlockProps } from '@/payload-types'
 
@@ -20,17 +19,12 @@ type Props = MediaBlockProps & {
 
 export const MediaBlock: React.FC<Props> = (props) => {
   const {
-    captionClassName,
     className,
     enableGutter = true,
     imgClassName,
     media,
     staticImage,
-    disableInnerContainer,
   } = props
-
-  let caption
-  if (media && typeof media === 'object') caption = media.caption
 
   return (
     <div
@@ -48,19 +42,6 @@ export const MediaBlock: React.FC<Props> = (props) => {
           resource={media}
           src={staticImage}
         />
-      )}
-      {caption && (
-        <div
-          className={cn(
-            'mt-6',
-            {
-              container: !disableInnerContainer,
-            },
-            captionClassName,
-          )}
-        >
-          <RichText data={caption} enableGutter={false} />
-        </div>
       )}
     </div>
   )
