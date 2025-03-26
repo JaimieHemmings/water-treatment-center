aeff9d6fff65b5af483f1ee29cd1f6db6ae5e82a
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-03-26 15:16:33 +0000

    Update Component.tsx

--- src/blocks/WideText/Component.tsx
+++ src/blocks/WideText/Component.tsx
@@ -18,26 +18,31 @@ export const WideTextBlock: React.FC<WideTextBlockProps> = ({
   titleHighlight,
   titleEnd,
   description, 
-  features 
+  features,
+  darkmode
 }) => {
+  darkmode = true;
   return (
-    <div className="py-[5rem] bg-white">
+    <div className={`py-[5rem] ${darkmode ? 'bg-darkblue' : 'bg-white'}`}>
       <div className="container">
-        <h2 className="text-2xl md:text-4xl text-textblue mb-4">
+        <h2
+          className={`${darkmode ? 'text-white' : 'text-textblue'} text-2xl md:text-4xl mb-4`}>
           {titleStart}{' '}
           <span className="text-selectiveyellow">{titleHighlight}</span>
           {' '}{titleEnd}
         </h2>
-        {description && <RichText data={description} enableGutter={false} className="text-textblue" />}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {features.map((feature, index) => (
-            <div key={index} className="bg-antiflashwhite p-6 rounded-lg">
-              <h3 className="text-xl text-textblue mb-3">
-                {feature.title}
-              </h3>
-              {feature.description && <RichText data={feature.description} enableGutter={false} className="text-textblue" />}
-            </div>
-          ))}
+          <div className={`${darkmode ? 'text-white' : 'text-textblue'} mb-8`}>
+            {description && <RichText data={description} enableGutter={false} />}
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-textblue">
+            {features.map((feature, index) => (
+              <div key={index} className="bg-antiflashwhite p-6 rounded-lg">
+                <h3 className="text-xl mb-3">
+                  {feature.title}
+                </h3>
+                {feature.description && <RichText data={feature.description} enableGutter={false} />}
+              </div>
+            ))}
         </div>
       </div>
     </div>
