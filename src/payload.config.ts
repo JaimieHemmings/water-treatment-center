17f9389b90e05ddbdb3519d5cf124a5f1c46e5c3
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-03-29 09:39:12 +0000

    update: send mail

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -105,5 +105,10 @@ export default buildConfig({
         region: process.env.S3_REGION,
       },
     }),
-  ]
+  ],
+  email: resendAdapter({
+    defaultFromAddress: process.env.EMAIL_FROM_ADDRESS,
+    defaultFromName: process.env.EMAIL_FROM_NAME,
+    apiKey: process.env.RESEND_API_KEY || '',
+  })
 })
