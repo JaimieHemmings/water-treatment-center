466f762c75a6e1ec5408958b391d602fa60a9bb1
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-03-29 09:53:02 +0000

    Update TestSubmissions.ts

--- src/collections/TestSubmissions.ts
+++ src/collections/TestSubmissions.ts
@@ -1,5 +1,4 @@
 import type { CollectionConfig } from 'payload'
-import { v4 as uuidv4 } from 'uuid'
 
 import { anyone } from '../access/anyone'
 import { authenticated } from '../access/authenticated'
@@ -17,26 +16,7 @@ const TestSubmissions: CollectionConfig = {
     defaultColumns: ['uuid', 'name', 'email', 'createdAt'],
     group: 'Form Submissions',
   },
-  hooks: {
-    beforeChange: [
-      ({ data }) => {
-        // Only set UUID if it's a new document
-        if (!data.uuid) {
-          data.uuid = uuidv4()
-        }
-        return data
-      },
-    ],
-  },
   fields: [
-    {
-      name: 'uuid',
-      type: 'text',
-      admin: {
-        readOnly: true,
-      },
-      unique: true,
-    },
     {
       name: 'name',
       type: 'text',
